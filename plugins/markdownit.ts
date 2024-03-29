96d6c765765e0bf1be7bc72321df162d08b0fc01
Jesse Wierzbinski <contact@cpluspatch.com>
2024-03-29 01:48:13 -1000

    Restrict imported themes by Shiki

--- plugins/markdownit.ts
+++ plugins/markdownit.ts
@@ -1,34 +1,40 @@
 import MarkdownIt from "markdown-it";
-import Shiki from "@shikijs/markdown-it";
 import markdownItTocDoneRight from "markdown-it-toc-done-right";
 import markdownItContainer from "markdown-it-container";
 import markdownItAnchor from "markdown-it-anchor";
 import markdownItTaskLists from "@hackmd/markdown-it-task-lists";
+import { fromHighlighter } from "@shikijs/markdown-it/core";
+import { getHighlighterCore } from "shiki/core";
 
-export default defineNuxtPlugin(async () => {
+const highlighter = await getHighlighterCore({
+	themes: [import("shiki/themes/rose-pine.mjs")],
+	langs: [
+		import("shiki/langs/javascript.mjs"),
+		import("shiki/langs/typescript.mjs"),
+		import("shiki/langs/python.mjs"),
+		import("shiki/langs/toml.mjs"),
+		import("shiki/langs/rust.mjs"),
+		import("shiki/langs/sql.mjs"),
+		import("shiki/langs/json.mjs"),
+		import("shiki/langs/html.mjs"),
+		import("shiki/langs/css.mjs"),
+		import("shiki/langs/scss.mjs"),
+		import("shiki/langs/bash.mjs"),
+		import("shiki/langs/shell.mjs"),
+		import("shiki/langs/yaml.mjs"),
+	],
+	loadWasm: import("shiki/wasm"),
+});
+
+export default defineNuxtPlugin(() => {
 	const renderer = MarkdownIt({
 		html: true,
 		linkify: true,
 	});
 
 	renderer.use(
-		await Shiki({
-			theme: "synthwave-84",
-			langs: [
-				"javascript",
-				"typescript",
-				"python",
-				"toml",
-				"rust",
-				"sql",
-				"json",
-				"html",
-				"css",
-				"scss",
-				"bash",
-				"shell",
-				"yaml",
-			],
+		fromHighlighter(highlighter as any, {
+			theme: "rose-pine",
 		})
 	);
 

fccf1ddb3df888feace87bc03fa277e17f134625
Jesse Wierzbinski <contact@cpluspatch.com>
2024-09-14 13:30:42 +0200

    fix: :bug: Fix incorrect script declaration

--- pages/money.vue
+++ pages/money.vue
@@ -1,6 +1,10 @@
-<script>
+<script setup lang="ts">
 // Redirect to https://buy.stripe.com/9AQeY83pn39k9u8005
 await navigateTo("https://buy.stripe.com/9AQeY83pn39k9u8005", {
     external: true,
 });
 </script>
+
+<template>
+
+</template>

a146a0de75470c42e71494184475d9d268942721
Jesse Wierzbinski <contact@cpluspatch.com>
2025-03-02 00:00:44 +0100

    fix: :bulb: Improve note for mobile users on jammin'

--- pages/jammin.vue
+++ pages/jammin.vue
@@ -14,7 +14,8 @@ const { width } = useWindowSize();
 			class="flex flex-col text-center items-center justify-center gap-5">
 			<Laptop class="text-white size-8" />
 			<p class="text-white text-sm">
-				Please visit this page on a desktop or tablet.
+				Please visit this page on a desktop or tablet, otherwise it
+				won't fit on your screen.
 			</p>
 		</div>
 	</div>

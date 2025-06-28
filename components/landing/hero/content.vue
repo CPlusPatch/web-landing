9f25afe475826830506d831103db3a5aea9622ea
Jesse Wierzbinski <contact@cpluspatch.com>
2025-06-28 03:57:04 +0200

    feat: :sparkles: Add virus

--- components/landing/hero/content.vue
+++ components/landing/hero/content.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { NuxtLink } from "#components";
 import Container from "~/components/containers/big.vue";
-import { Button } from "~/components/ui/button";
+import Virus from "./virus.vue";
 
 defineProps<{
     isJesse: boolean;
@@ -24,9 +23,9 @@ const openInNewTab = (url: string) => window.open(url, "_blank");
                 many kinds of fields
             </p>
             <div class="mt-10 flex items-center gap-4">
-                <Button href="/contact" :as="NuxtLink">
-                    Contact me
-                </Button>
+                <ClientOnly>
+                    <Virus />
+                </ClientOnly>
             </div>
         </div>
         <div class="mt-16 flex sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
---
+++ components/landing/hero/virus.vue
@@ -0,0 +1,16 @@
+<template>
+    <Button @click="memz.all()">
+        <Worm />
+        I want a virus!
+    </Button>
+</template>
+
+<script lang="ts" setup>
+import Memz from "@skwalexe/memz";
+import { Worm } from "lucide-vue-next";
+import { Button } from "~/components/ui/button";
+
+const memz = new Memz(
+    "https://cdn.jsdelivr.net/gh/SkwalExe/Memz.js@0.1.0/src/assets/",
+);
+</script>
